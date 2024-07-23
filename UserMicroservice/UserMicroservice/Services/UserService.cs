dd7345ce66bff2e0b8478eac22e7c782b63cfe19
Evewibe <aleksey.dantist@gmail.com>
2024-07-23 16:49:49 +0600

    feat: user-proto

---
+++ UserMicroservice/UserMicroservice/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+
+namespace UserMicroservice.Services;
+
+public class UserService : User.UserBase
+{
+    public override async Task<CreateReply> CreateUser(CreateRequest request, ServerCallContext context)
+    {
+        return new CreateReply
+        {
+            Message = $"Success create user with login: {request.Login}"
+        };
+    }
+}
--- UserMicroservice/UserMicroservice/obj/Debug/net8.0/UserMicroservice.AssemblyInfo.cs
+++ UserMicroservice/UserMicroservice/obj/Debug/net8.0/UserMicroservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48a75901eac46e4b16dbda5817d71e25645492a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb9965ad8133a98f3687c58aa0cd3db9ac0be74")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
