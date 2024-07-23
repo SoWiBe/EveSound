8c961b8e9abbc7801ad535d75e4c4fa38b7b314f
Evewibe <aleksey.dantist@gmail.com>
2024-07-23 17:04:57 +0600

    feat: crud in users service

--- UserMicroservice/UserMicroservice/Services/UserService.cs
+++ UserMicroservice/UserMicroservice/Services/UserService.cs
@@ -11,4 +11,19 @@ public class UserService : User.UserBase
             Message = $"Success create user with login: {request.Login}"
         };
     }
+
+    public override Task<GetAllReply> GetUsers(GetAllRequest request, ServerCallContext context)
+    {
+        return base.GetUsers(request, context);
+    }
+
+    public override Task<UpdateReply> UpdateUser(UpdateRequest request, ServerCallContext context)
+    {
+        return base.UpdateUser(request, context);
+    }
+
+    public override Task<DeleteReply> DeleteUser(DeleteRequest request, ServerCallContext context)
+    {
+        return base.DeleteUser(request, context);
+    }
 }
