9f2e5a289ad92d0171b43f104d2656aa257fe3a8
Evewibe <aleksey.dantist@gmail.com>
2024-07-24 18:47:42 +0600

    feat: test client

---
+++ TestClient/TestClient/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Grpc.Net.Client;
+using TestClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7054/");
+var client = new User.UserClient(channel);
+Console.Write("Write name: ");
+var name = Console.ReadLine();
+
+var reply = await client.CreateUserAsync(new CreateRequest()
+{
+    Login = name,
+    Email = "test",
+    Password = "test"
+});
+
+Console.Write($"Server response: {reply.Message}");
