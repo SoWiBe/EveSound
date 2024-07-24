33224d7ee61c0cc63be32c2c6128733f5730f4ab
Evewibe <aleksey.dantist@gmail.com>
2024-07-24 18:54:23 +0600

    feat: init music microservice

---
+++ MusicMicroservice/src/main/java/com/example/MusicMicroservice/MusicMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMicroserviceApplication.class, args);
+	}
+
+}
---
+++ MusicMicroservice/src/test/java/com/example/MusicMicroservice/MusicMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
