0f363a5acb056246d33c439a6a15e55be6d927d5
Evewibe <aleksey.dantist@gmail.com>
2024-08-01 16:44:26 +0600

    feat: MusicController.java

--- MusicMicroservice/src/main/java/com/example/MusicMicroservice/controllers/MusicController.java
+++ MusicMicroservice/src/main/java/com/example/MusicMicroservice/controllers/MusicController.java
@@ -1,6 +1,8 @@
 package com.example.MusicMicroservice.controllers;
 
 import infrastructure.models.Music;
+import infrastructure.services.MusicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,13 +12,21 @@ import java.util.List;
 @RequestMapping(value = "/api/v1/music")
 public class MusicController {
 
-    @GetMapping
-    public Music getMusic(){
-        return new Music("qwe","ewq","autor","uuu",1223);
-    }
+    @Autowired
+    private MusicService musicService;
 
     @PostMapping
-    public int getNumber(){
-        return 1;
+    public Music createMusic(@RequestBody Music music){
+        return musicService.createMusic(music);
+    }
+
+    @GetMapping("/{id}")
+    public Music getMusicById(@PathVariable String id){
+        return musicService.getMusicById(id).orElse(null);
+    }
+
+    @GetMapping
+    public List<Music> getMusic() {
+        return musicService.getAllMusic();
     }
 }
