a1631266cede7af4e9f24787639c5b25372d72e2
Evewibe <aleksey.dantist@gmail.com>
2024-08-01 16:41:16 +0600

    feat: Music.java and MusicService.java

---
+++ MusicMicroservice/src/main/java/infrastructure/services/MusicService.java
@@ -0,0 +1,47 @@
+package infrastructure.services;
+
+import abstractions.repository.IMusicRepository;
+import abstractions.services.IMusicService;
+import infrastructure.models.Music;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MusicService implements IMusicService {
+
+    private final IMusicRepository musicRepository;
+
+    @Autowired
+    public MusicService(IMusicRepository musicRepository) {
+        this.musicRepository = musicRepository;
+    }
+
+    @Override
+    public Music createMusic(Music music) {
+        return musicRepository.save(music);
+    }
+
+    @Override
+    public List<Music> getAllMusic() {
+        return musicRepository.findAll();
+    }
+
+    @Override
+    public Optional<Music> getMusicById(String id) {
+        return musicRepository.findById(id);
+    }
+
+    @Override
+    public void deleteMusic(String id) {
+        // need to realize
+    }
+
+    @Override
+    public Music updateMusic(Music music) {
+        // need to realize
+        return new Music();
+    }
+}
