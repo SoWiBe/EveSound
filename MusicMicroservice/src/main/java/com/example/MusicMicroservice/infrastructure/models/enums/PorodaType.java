7aece3e8e27efad63cb80fb11b221d256d808833
Evewibe <aleksey.dantist@gmail.com>
2024-08-01 17:08:08 +0600

    feat: remove Animal.java, PorodaType.java and Sex.java

--- MusicMicroservice/src/main/java/com/example/MusicMicroservice/infrastructure/models/enums/PorodaType.java
+++
@@ -1,6 +0,0 @@
-package com.example.MusicMicroservice.infrastructure.models.enums;
-
-public enum PorodaType {
-    Porodistaya,
-    Neporodistaya
-}
--- MusicMicroservice/src/main/java/com/example/MusicMicroservice/infrastructure/models/enums/Sex.java
+++
@@ -1,6 +0,0 @@
-package com.example.MusicMicroservice.infrastructure.models.enums;
-
-public enum  Sex {
-    Male,
-    Female
-}
